edeb84915627ff0502322735a2340028a9671fa3
Zargerion <iolegx7@gmail.com>
2023-04-02 08:21:11 +0300

    colored menu

--- cli/interface.py
+++ cli/interface.py
@@ -8,7 +8,11 @@ class MainMenu:
     @abstractmethod
     def map():
         while True:
-            print(Fore.BLUE + 'Синий текст')
+            print(Fore.YELLOW + 'Element 1')
+            print(Fore.YELLOW + 'Element 2')
+            print(Fore.YELLOW + 'Element 3')
+            print(Fore.YELLOW + 'Element 4')
+            print(Fore.YELLOW + 'Element 5')
             time.sleep(1)
             os.system("cls||clear")
     
