2ca4d4cd1c12e2aab87a3150ebbf5361b0ba021f
Zargerion <iolegx7@gmail.com>
2023-04-02 10:40:55 +0300

    Nice look 2 concept

--- cli/interface.py
+++ cli/interface.py
@@ -3,7 +3,7 @@ import os
 import time
 from colorama import Fore
 
-DESCRIPTION = "Press W, E keys to change menu options. Enter to select option and Esc for close the programm."
+MAIN_DESCRIPTION = "Press W, E keys to change menu options. Enter to select an option and Esc to close the programm."
 ENDL = "\n"
 SPACE = " "
 
@@ -12,12 +12,14 @@ class MainMenu:
     def map():
         while True:
             os.system("cls||clear")
-            print(ENDL + SPACE + Fore.LIGHTRED_EX + DESCRIPTION + "\n\n")
+            print(ENDL + SPACE + Fore.LIGHTRED_EX + MAIN_DESCRIPTION + "\n\n")
             print(SPACE + Fore.YELLOW + 'Element 1')
             print(SPACE + Fore.YELLOW + 'Element 2')
             print(SPACE + Fore.LIGHTYELLOW_EX + '\033[4m' + '_Element 3' + '\033[0m')
             print(SPACE + Fore.YELLOW + 'Element 4')
             print(SPACE + Fore.YELLOW + 'Element 5')
+            element_desc = "That's doing something for anything..."
+            print(ENDL + ENDL + SPACE + Fore.YELLOW + 'Option\'s description: ' + element_desc)
             time.sleep(1)
             
     
