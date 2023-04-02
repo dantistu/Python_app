6ee1217c6f783e65a0d52e30ab92749e28b2f15e
Zargerion <iolegx7@gmail.com>
2023-04-02 08:00:48 +0300

    bug fix 1

--- cli/interface.py
+++ cli/interface.py
@@ -1,13 +1,14 @@
 from abc import abstractmethod
 import os
 import time
+from colorama import Fore
 
 
 class MainMenu: 
     @abstractmethod
     def map():
         while True:
-            print("menu")
+            print(Fore.BLUE + 'Синий текст')
             time.sleep(1)
             os.system("cls||clear")
     
